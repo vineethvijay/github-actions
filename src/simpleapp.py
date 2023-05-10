1f45cd508bb5f030bca4ce23487e9c9c53bb1d59
Vineeth Vijayan <vineethvijay777@gmail.com>
2023-05-10 20:43:41 +0200

    lint 2

--- src/simpleapp.py
+++ src/simpleapp.py
@@ -13,6 +13,4 @@ def hello_world():
     return "Hello world!!"
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser(usage="python simpleapp.py -p ")
-    parser.add_option('-p', '--port', action='store', dest='port', help='The port to listen on.')
     app.run(host='0.0.0.0', port=8000, debug=False)
